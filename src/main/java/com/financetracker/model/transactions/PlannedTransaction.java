bf76952873163a8d8a61c231835abb3b8b4c44e1
Dzhanan Salim <cani_1991@abv.bg>
2018-04-30 09:35:42 +0300

    Created classes Transaction, TransactionDAO,TransactionException and TransactionServlet

---
+++ src/main/java/com/financetracker/model/transactions/PlannedTransaction.java
@@ -0,0 +1,5 @@
+package com.financetracker.model.transactions;
+
+public class PlannedTransaction {
+
+}
--- src/main/java/com/financetracker/model/transactions/Transaction.java
+++ src/main/java/com/financetracker/model/transactions/Transaction.java
@@ -1,5 +1,119 @@
 package com.financetracker.model.transactions;
 
+import java.time.LocalDate;
+
+import com.financetracker.exceptions.TransactionException;
+
 public class Transaction {
+	private int id;
+	private String payee;
+	private double amount;
+	private LocalDate date;
+	private int accountID;
+	private boolean isIncome;
+	private int category;
+	
+	public Transaction() {
+		
+	}
+
+	public Transaction(String payee, double amount,LocalDate date, int account, boolean isIncome, int category)
+			throws TransactionException {
+		setPayee(payee);
+		setAmount(amount);
+		setDate(date);
+		setAccount(account);
+		this.isIncome = isIncome;
+		setCategory(category);
+	}
+
+	public boolean isValidString(String string) {
+		if (string != null && string.trim().length() > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) throws TransactionException {
+		if (id > 0) {
+			this.id = id;
+		} else {
+			throw new TransactionException("Invalid id!");
+		}
+	}
+
+	public String getPayee() {
+		return payee;
+	}
+
+	public void setPayee(String payee) throws TransactionException {
+		if (isValidString(payee)) {
+			this.payee = payee;
+		} else {
+			throw new TransactionException("Invalid payee or payer!");
+		}
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) throws TransactionException {
+		if (amount > 0) {
+			this.amount = amount;
+		}else {
+			throw new TransactionException("Invalid amount for transaction!");
+		}
+		
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	private void setDate(LocalDate date) throws TransactionException {
+		if(date != null) {
+			this.date = date;
+		} else {
+			throw new TransactionException("Invalid date!");
+		}
+	}
+
+	public int getAccountID() {
+		return accountID;
+	}
+
+	public void setAccount(int accountID) throws TransactionException {
+		if (accountID > 0) {
+			this.accountID = accountID;
+		}else {
+			throw new TransactionException("Invalid account!");
+		}
+
+	}
+
+	public boolean isIncome() {
+		return isIncome;
+	}
+
+	public int getCategory() {
+		return category;
+	}
+
+	public void setCategory(int category) throws TransactionException {
+		if (category > 0) {
+			this.category = category;
+		}else {
+			throw new TransactionException("Invalid category!");
+		}
+	
+	}
+
+	
+	
 
 }
